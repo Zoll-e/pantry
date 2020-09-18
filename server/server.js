31a93a21cca212d0d3030befda717e06eeb8d638
Irilina <debrecenizoltan19@gmail.com>
2020-09-18 20:24:34 +0200

    updated config

--- server/server.js
+++ server/server.js
@@ -1,29 +1,27 @@
 const express = require("express");
-const passport = require("passport")
+const passport = require("passport");
+const connectDB = require("./config/db");
+
 const app = express();
 
-app.use('/uploads',express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
-
-// Our DB Configuration
-require('./src/database');
+// Connect db
+connectDB();
 
 // Json middleware
-app.use(express.json({extended:true}));
+app.use(express.json({ extended: true }));
 
-app.get('/',(req,res)=>{res.send("csa")});
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-app.use('/api/user', require('./src/api/routes/user'));
-app.use('/api/profile', require('./src/api/routes/profile'));
-app.use('/api/recipe', require('./src/api/routes/recipe'));
-app.use('/api/picture', require('./src/api/routes/picture'));
-
-
+app.use("/api/user", require("./src/api/routes/user"));
+app.use("/api/profile", require("./src/api/routes/profile"));
+app.use("/api/recipe", require("./src/api/routes/recipe"));
+app.use("/api/picture", require("./src/api/routes/picture"));
 
 const port = 5000;
 
