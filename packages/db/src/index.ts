071855848562ff8a89c9fd4e1b13c7b069002f97
Zoll-e <55623473+Zoll-e@users.noreply.github.com>
2026-03-01 15:25:24 +0100

    fest: first controller (#5)

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,10 @@
 import { SQL } from 'bun';
 import * as schema from './schema.js';
-import { drizzle } from 'drizzle-orm/singlestore/driver';
+import { drizzle } from 'drizzle-orm/bun-sql';
+import { config } from 'dotenv';
+
+config({ path: '../../.env' });
 
 const client = new SQL(process.env.DATABASE_URL!);
 
-export const db = drizzle(client, { schema });
+export const db = drizzle({ client, schema });
